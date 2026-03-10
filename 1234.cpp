bd8a3519310b5cae19988dbca12bbe3cb88e71c4
Choi Woong <yyamoo.oo@gmail.com>
2026-03-10 13:52:44 +0900

    first commit

---
+++ 1234.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num[3] = {10,20,30};
+
+    for(int i = 0; i < 3; i++)
+    {
+        cout << num[i] << endl;
+    }
+
+    return 0;
+}
