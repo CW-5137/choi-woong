cb98de857a3f276a68ad44981cda96a2a71f68d2
Choi Woong <yyamoo.oo@gmail.com>
2026-03-11 19:19:46 +0900

    ascii art homework

---
+++ love.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "W       W   OOOOO   OOOOO   N   N   GGGGG" << endl;
+    cout << "W       W  O     O O     O  NN  N  G" << endl;
+    cout << "W   W   W  O     O O     O  N N N  G  GGG" << endl;
+    cout << " W W W W   O     O O     O  N  NN  G    G" << endl;
+    cout << "  W   W     OOOOO   OOOOO   N   N   GGGGG" << endl;
+
+    cout << endl;
+
+    cout << "        /\\_/\\ " << endl;
+    cout << "       ( o.o )" << endl;
+    cout << "        > ^ <" << endl;
+
+    return 0;
+}
